38029d499888c6ed1d72839f75f382af6711d066
thauany-alves <thauanyaraujoalves@hotmail.com>
2021-09-23 17:48:15 -0300

    Implementing unit test

--- src/pages/Login/test.spec.js
+++ src/pages/Login/test.spec.js
@@ -1,11 +1,11 @@
 import { waitFor , fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
-import { LoginFormAntd } from '.';
+import { Login} from '.';
 
 const setup = () => {
-  const utils = render(<LoginFormAntd />);
+  const utils = render(<Login />);
   const userInput = screen.getByRole('textbox');
-  const passwordInput = screen.getByRole('password');
+  const passwordInput = screen.getByPlaceholderText(/password/i);
   const button = screen.getByRole('button', { name: /Log in/i });
   return {
     userInput,
@@ -29,7 +29,7 @@ test('Show fields page', () => {
 
 test('Should input values and call submit', async () => {
   const { userInput, passwordInput, button } = setup();
-  const onFinish = jest.fn();
+  // const onFinish = jest.fn();
 
   fireEvent.change(userInput, { target: { value: 'username' } });
   fireEvent.change(passwordInput, { target: { value: 'password' } });
