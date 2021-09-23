6637324d4d801916d7cfb77810ceddd9b27fc667
thauany-alves <thauanyaraujoalves@hotmail.com>
2021-09-23 09:53:14 -0300

    Fix identation

--- src/pages/RegisterPassword/index.js
+++ src/pages/RegisterPassword/index.js
@@ -2,76 +2,76 @@ import { Form, Input } from 'antd';
 import * as S from './styles';
 
 export function RegisterPasswordPage(){
-	const onFinish = (values) => {
+  const onFinish = (values) => {
     console.log('Received values of form: ', values);
   };
 
-	return(
-		<S.Container>
-			<S.ContainerForm>
-				<h2>Register Password</h2>
+  return(
+    <S.Container>
+      <S.ContainerForm>
+        <h2>Register Password</h2>
 
-				<Form 
-					name="register"
-					layout="vertical"
-					size="large"
-					onFinish={onFinish}
-				>
-					<Form.Item
-						name="current"
-						rules={[
-							{
-								required: true,
-								message: 'Please input your current password!',
-							},
-						]}
-						hasFeedback
-					>
-						<Input.Password placeholder="Current Password"/>
-					</Form.Item>
-					
-					<Form.Item
-						name="password"
-						rules={[
-							{
-								required: true,
-								message: 'Please input your password!',
-							},
-						]}
-						hasFeedback
-					>
-						<Input.Password placeholder="Password"/>
-					</Form.Item>
+        <Form
+          name="register"
+          layout="vertical"
+          size="large"
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="current"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your current password!',
+              },
+            ]}
+            hasFeedback
+          >
+            <Input.Password placeholder="Current Password"/>
+          </Form.Item>
+          
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+            hasFeedback
+          >
+            <Input.Password placeholder="Password"/>
+          </Form.Item>
 
-					<Form.Item
-						name="confirm"
-						dependencies={['password']}
-						hasFeedback
-						rules={[
-							{
-								required: true,
-								message: 'Please confirm your password!',
-							},
-							({ getFieldValue }) => ({
-								validator(_, value) {
-									if (!value || getFieldValue('password') === value) {
-										return Promise.resolve();
-									}
-									return Promise.reject(new Error('The two passwords that you entered do not match!'));
-								},
-							}),
-						]}
-					>
-						<Input.Password placeholder="Confirm Password"/>
-					</Form.Item>
+          <Form.Item
+            name="confirm"
+            dependencies={['password']}
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: 'Please confirm your password!',
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                },
+              }),
+            ]}
+          >
+            <Input.Password placeholder="Confirm Password"/>
+          </Form.Item>
 
-					<Form.Item>
+          <Form.Item>
             <S.Button type="primary" htmlType="submit" className="login-form-button">
               Save
             </S.Button>
           </Form.Item>
-				</Form>
-			</S.ContainerForm>
-		</S.Container>
-	);
+        </Form>
+      </S.ContainerForm>
+    </S.Container>
+  );
 }
